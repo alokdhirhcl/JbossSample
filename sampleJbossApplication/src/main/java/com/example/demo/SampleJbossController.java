5bfe944b335028f7adadf46bbaaf26c5711d36f8
azureadmin <azureadmin@host.docker.internal>
2024-01-12 08:07:27 +0000

    Initial commit

---
+++ sampleJbossApplication/src/main/java/com/example/demo/SampleJbossController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class SampleJbossController {
+
+	
+	 @GetMapping("/welcome")
+     public String welcome() {
+        
+         return "Hello welcome to new spring boot App ";
+     }
+}
